03d17ce1998d5e15d9ad172d41ba14977caa4bb0
Vibi31 <vrvichu0@gmail.com>
2022-02-06 14:53:17 +0100

    update

--- FYS2150/prelab2.py
+++ FYS2150/prelab2.py
@@ -1,3 +1,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import linregress
+
+file = open('RC_data.csv', 'r')
+data = file.read()
+#data = np.loadtxt('RC_data.csv')   """this method did not work"""
+
+print(data)  #testing
+
+frekvens = []
+vu_vi = []
+
+with open('RC_data.csv', 'r') as file:
+    firstline = next(file)
+    for line in file:
+        values = line.split(',')
+        frekvens.append(values[0])
+        vu_vi.append(values[1])
+
+
+"""
+for line in data:
+    values = line.split(',')
+    print(values)
+    frekvens.append(values[0])
+    vu_vi.append(values[1])
+    #frekven =
+"""
+#last 9 values:
+nine_frekvens = np.array(frekvens[-9:])
+nine_vuvi = np.array(vu_vi[-9:])
+
+print(linregress(nine_vuvi))
+
+
+
+
 
